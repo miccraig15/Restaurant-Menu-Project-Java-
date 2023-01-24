bf2d16a589d7038e91e8ab1bb5b6a3e6f4b046fb
miccraig15 <miccraig15@gmail.com>
2023-01-24 16:57:07 -0600

    Completed Menu and MenuItem classes

--- src/main/java/restaurant/Menu.java
+++ src/main/java/restaurant/Menu.java
@@ -1,6 +1,25 @@
 package restaurant;
 
+import java.util.ArrayList;
 import java.util.Date;
 
 public class Menu {
+    private ArrayList updated;
+    private ArrayList menuItems;
+    private ArrayList newMenuItems;
+
+    public Menu(ArrayList updated, ArrayList menuItems, ArrayList newMenuItems) {
+        this.updated = updated;
+        this.menuItems = menuItems;
+        this.newMenuItems = newMenuItems;
+    }
+
+    public void setUpdated(ArrayList updated) {this.updated=updated;}
+    public void setMenuItems(ArrayList menuItems) {this.menuItems=menuItems;}
+    public void setNewMenuItems(ArrayList newMenuItems) {this.newMenuItems=newMenuItems;}
+
+    public ArrayList getupdated() { return updated; }
+    public ArrayList getmenuItems() { return menuItems; }
+    public ArrayList getNewMenuItems() { return newMenuItems; }
+
 }
--- src/main/java/restaurant/MenuItem.java
+++ src/main/java/restaurant/MenuItem.java
@@ -1,28 +1,34 @@
 package restaurant;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 public class MenuItem {
     private String name;
     private String description;
     private String category;
     private double price;
+    private ArrayList Date;
 
 
-    public MenuItem(String name, String description, String category, double price) {
+    public MenuItem(String name, String description, String category, double price, ArrayList Date) {
         this.name = name;
         this.description = description;
         this.category = category;
         this.price = price;
+        this.Date = Date;
     }
 
     public void setName(String name) {this.name=name;}
     public void setDescription(String description) {this.description=description;}
     public void setCategory(String category) {this.category=category;}
     public void setPrice(double price) {this.price=price;}
+    public void setDate(ArrayList Date) {this.Date=Date;}
 
     public String getName() { return name; }
     public String getDescription() { return description; }
     public String getCategory() { return category; }
     public double getPrice() { return price; }
-
+    public ArrayList getDate() { return Date; }
 
 }
